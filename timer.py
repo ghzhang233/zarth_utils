5cd930172d2e441784ae06d3daf82b5ea878ded6
ghzhang <zhangguanhua1996@gmail.com>
2021-04-09 19:01:16 +0800

    update timer

--- timer.py
+++ timer.py
@@ -13,22 +13,34 @@ class Timer:
     def start(self):
         self.start_time = time.time()
 
+    def is_started(self):
+        return self.start_time is not None
+
+    def is_ended(self):
+        return self.start_time is None
+
     def end(self):
-        if self.start_time is not None:
+        if self.is_started():
             self.last_duration = time.time() - self.start_time
             self.cumulative_duration += self.last_duration
             self.start_time = None
 
-    def get_last_duration(self, start_again=False):
-        if self.start_time is not None:
-            self.end()
+    def get_last_duration(self, end_first=False, start_again=False):
+        if not self.is_ended():
+            if end_first:
+                self.end()
+            else:
+                return -1
         if start_again:
             self.start()
         return self.last_duration
 
-    def get_cumulative_duration(self, start_again=False):
-        if self.start_time is not None:
-            self.end()
+    def get_cumulative_duration(self, end_first=False, start_again=False):
+        if not self.is_ended():
+            if end_first:
+                self.end()
+            else:
+                return -1
         if start_again:
             self.start()
         return self.cumulative_duration
