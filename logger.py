69c37627f261835f01e975144e37b46f96f5b31e
ghzhang <zhangguanhua1996@gmail.com>
2021-04-23 01:45:11 +0800

    update logger

--- logger.py
+++ logger.py
@@ -10,16 +10,18 @@ if not os.path.exists(dir_logs):
 def get_logger(file_log=os.path.join(dir_logs, "%s.log" % get_random_time_stamp())):
     ret_logger = logging.getLogger()
     ret_logger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    formatter = logging.Formatter('%(asctime)s-%(name)s-%(levelname)s: %(message)s', datefmt='%Y-%m-%d-%H:%M:%S')
 
-    fh = logging.FileHandler(file_log)
-    fh.setLevel(logging.DEBUG)
-    fh.setFormatter(formatter)
+    if not ret_logger.handlers:
+        fh = logging.FileHandler(file_log)
+        fh.setLevel(logging.DEBUG)
+        fh.setFormatter(formatter)
 
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-    ch.setFormatter(formatter)
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+        ch.setFormatter(formatter)
 
-    ret_logger.addHandler(ch)
-    ret_logger.addHandler(fh)
+        ret_logger.addHandler(ch)
+        ret_logger.addHandler(fh)
+
     return ret_logger
