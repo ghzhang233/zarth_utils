e2c58a0f92d65d935fdc24c369e2bb3648f2d3ff
ghzhang <zhangguanhua1996@gmail.com>
2021-04-23 02:54:31 +0800

    update config.py

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
 import json
 import argparse
 
+from general_utils import get_random_time_stamp
 from logger import get_logger
 
 
 class Config:
-    def __init__(self, default_config_filename="default_config.json"):
+    def __init__(self, dump_file_name="%s.config" % get_random_time_stamp(),
+                 default_config_filename="default_config.json"):
         self.__parameters = {}
-
         # load from default config file
         self.__parameters.update(json.load(open(default_config_filename, "r", encoding="utf-8")))
 
@@ -17,6 +18,9 @@ class Config:
             parser.add_argument("--%s" % name_param, default=self.__parameters[name_param])
         args = parser.parse_args()
         self.__parameters.update(vars(args))
+
+        json.dump(self.__parameters, open(dump_file_name, "w", encoding="utf-8"))
+
         self.__logger = get_logger()
 
     def __getitem__(self, item):
@@ -25,9 +29,5 @@ class Config:
     def to_dict(self):
         return self.__parameters
 
-    def dump(self, filename):
-        json.dump(self.__parameters, open(filename, "w", encoding="utf-8"))
-
     def show(self):
-        for key in self.__parameters.keys():
-            self.__logger.info("%s: %s" % (key, str(self.__parameters[key])))
+        self.__logger.info("\n%s" % json.dumps(self.__parameters, sort_keys=True, indent=4, separators=(',', ': ')))
