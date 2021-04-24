f805028d2c4b45ba58e378bef475e443050932ca
ghzhang <zhangguanhua1996@gmail.com>
2021-04-25 01:15:35 +0800

    fix a bug

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
+import os
+import sys
 import json
 import argparse
 
 from zarth_utils.general_utils import get_random_time_stamp
 from zarth_utils.logger import get_logger
+
+dir_configs = os.path.join(sys.path[0], "configs")
+if not os.path.exists(dir_configs):
+    os.makedirs(dir_configs)
 
 
 class Config:
@@ -16,12 +22,17 @@ class Config:
         # add argument parser
         parser = argparse.ArgumentParser()
         for name_param in self.__parameters.keys():
-            parser.add_argument("--%s" % name_param, default=self.__parameters[name_param])
+            value_param = self.__parameters[name_param]
+            if type(value_param) is bool:
+                parser.add_argument("--%s" % name_param, action="store_true", default=value_param)
+                parser.add_argument("--no-%s" % name_param, dest="%s" % name_param, action="store_false")
+            else:
+                parser.add_argument("--%s" % name_param, type=type(value_param), default=value_param)
         args = parser.parse_args()
         self.__parameters.update(vars(args))
 
         filename_dump = "%s.config" % filename_dump if not filename_dump.endswith(".config") else filename_dump
-        json.dump(self.__parameters, open(filename_dump, "w", encoding="utf-8"))
+        json.dump(self.__parameters, open(os.path.join(dir_configs, filename_dump), "w", encoding="utf-8"))
 
         self.__logger = get_logger()
 
