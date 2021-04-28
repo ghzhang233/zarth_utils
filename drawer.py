c424faee3fdc8d1784a38c75021a312445a4e8d3
ghzhang <zhangguanhua1996@gmail.com>
2021-04-28 19:16:54 +0800

    fix bugs

--- drawer.py
+++ drawer.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
 
 from zarth_utils.general_utils import get_random_time_stamp, makedir_if_not_exist
 
@@ -10,18 +10,18 @@ makedir_if_not_exist(dir_figures)
 
 
 class Drawer:
-    def __init__(self):
-        self.figure = plt.figure()
+    def __init__(self, figsize=(6.4, 4.8)):
+        self.figure = Figure(figsize=figsize)
 
-    def draw_one_axes(self, x, y, labels=None, *, nrows=1, ncols=1, index=1, title="", xlabel="", ylabel=""):
+    def draw_one_axes(self, x, y, labels=None, *, nrows=1, ncols=1, index=1, title="", xlabel="", ylabel="",
+                      use_marker=False):
         ax = self.figure.add_subplot(nrows, ncols, index)
 
-        format_generator = self.get_format()
+        format_generator = self.get_format(use_marker)
         for i, yi in enumerate(y):
             fmt = next(format_generator)
-            print(fmt)
             if labels is not None:
-                ax.plot(x, yi, fmt, labels=labels[i])
+                ax.plot(x, yi, fmt, label=labels[i])
             else:
                 ax.plot(x, yi, fmt)
 
@@ -34,7 +34,9 @@ class Drawer:
     def show(self):
         self.figure.show()
 
-    def save(self, fname=get_random_time_stamp()):
+    def save(self, fname=None):
+        if fname is None:
+            fname = get_random_time_stamp()
         fname = "%s.jpeg" % fname if not fname.endswith(".config") else fname
         self.figure.savefig(os.path.join(dir_figures, fname))
 
@@ -42,12 +44,14 @@ class Drawer:
         self.figure.clf()
 
     @staticmethod
-    def get_format():
+    def get_format(use_marker=False):
         p_color, p_style, p_marker = 0, 0, 0
         colors = ['r', 'g', 'b', 'c', 'm', 'y', 'k', 'w']
         styles = ['-', '--', '-.', ':']
-        markers = ['.', ',', 'o', 'v', '^', '<', '>', '1', '2', '3', '4', '8', 's', 'p', 'P', '*', 'h', 'H', '+', 'x',
-                   'X', 'D', 'd', '|', '_', ]
+        markers = [""]
+        if use_marker:
+            markers += ['.', ',', 'o', 'v', '^', '<', '>', '1', '2', '3', '4', '8', 's', 'p', 'P', '*', 'h', 'H', '+',
+                        'x', 'X', 'D', 'd', '|', '_', ]
 
         while True:
             yield colors[p_color] + styles[p_style] + markers[p_marker]
