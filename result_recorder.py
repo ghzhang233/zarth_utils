6ed9738258da48cc343546c8f2fd382c0f9f5f2a
ghzhang <zhangguanhua1996@gmail.com>
2022-05-14 00:19:41 +0800

    fix a bug

--- result_recorder.py
+++ result_recorder.py
@@ -275,5 +275,5 @@ def remove_duplicate(data, keys=("phase", "exp_name")):
 
 
 def merge_phase(data, data_to_merge, merge_on_keys=("exp_name",), suffixes=("", "_eval")):
-    data.merge(data_to_merge, how="left", on=merge_on_keys, suffixes=suffixes)
+    data.merge(data_to_merge, how="outer", on=merge_on_keys, suffixes=suffixes)
     return data
