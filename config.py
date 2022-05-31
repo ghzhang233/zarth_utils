e4384d7bdb2c2eb3b3cd8686f08f306494b19779
ghzhang <zhangguanhua1996@gmail.com>
2022-05-31 18:17:37 +0800

    update config

--- config.py
+++ config.py
@@ -15,13 +15,13 @@ class Config:
                  use_argparse=True):
         """
         Initialize the config. Note that either default_config_dict or default_config_file in json format must be
-        provided! If both are provided, only the dict will be used. The keys will be transferred to
-        argument names, and the type will be automatically detected.
-        The priority is the user specified parameter > user specified config file > default config file
+        provided! If both are provided, only the dict will be used. The keys will be transferred to argument names,
+        and the type will be automatically detected. The priority is the user specified parameter (if the use_argparse
+        is True) > user specified config file > default config dict > default config file.
 
         Examples:
         default_config_dict = {"lr": 0.01, "optimizer": "sgd", "num_epoch": 30, "use_early_stop": False}
-        Then the following corresponding arguments will be added in this function:
+        Then the following corresponding arguments will be added in this function if use_argparse is True:
         parser.add_argument("--lr", type=float)
         parser.add_argument("--optimizer", type=str)
         parser.add_argument("--num_epoch", type=int)
@@ -41,10 +41,10 @@ class Config:
         else:
             self.__parameters.update(json.load(open(default_config_file, "r", encoding="utf-8")))
 
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--config_file", type=str, default=None)
         if use_argparse:
             # add argument parser
-            parser = argparse.ArgumentParser()
-            parser.add_argument("--config_file", type=str, default=None)
             for name_param in self.__parameters.keys():
                 value_param = self.__parameters[name_param]
                 if type(value_param) is bool:
@@ -54,18 +54,22 @@ class Config:
                     parser.add_argument("--%s" % name_param, type=type(value_param[0]), default=value_param, nargs="+")
                 else:
                     parser.add_argument("--%s" % name_param, type=type(value_param), default=value_param)
-            args = parser.parse_args()
+        args = parser.parse_args()
 
+        if args.config_file is not None:
+            self.__parameters.update(json.load(open(args.config_file, "r", encoding="utf-8")))
+
+        if use_argparse:
             updated_parameters = dict()
             args_dict = vars(args)
             for k in vars(args):
                 if k != "config_file" and self.__parameters[k] != args_dict[k]:
                     updated_parameters[k] = args_dict[k]
-
-            if args.config_file is not None:
-                self.__parameters.update(json.load(open(args.config_file, "r", encoding="utf-8")))
-
             self.__parameters.update(updated_parameters)
+
+        for k in self.__parameters.keys():
+            assert k != "__parameters"
+            setattr(self, k, self.__parameters[k])
 
     def __getitem__(self, item):
         """
@@ -105,3 +109,8 @@ class Config:
 
     def keys(self):
         return self.__parameters.keys()
+
+
+def argparse2config(args):
+    args_dict = vars(args)
+    return Config(default_config_dict=args_dict, use_argparse=False)
