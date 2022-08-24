e956c3c47c3c5a9db87adea03f894152a36f7550
ghzhang <zhangguanhua1996@gmail.com>
2022-08-24 15:10:30 +0800

    add force_add_handler for logger

--- logger.py
+++ logger.py
@@ -4,16 +4,20 @@ import sys
 from zarth_utils.general_utils import get_random_time_stamp
 
 
-def get_logger(path_log="%s.log" % get_random_time_stamp()):
+def get_logger(path_log="%s.log" % get_random_time_stamp(), force_add_handler=False):
     """
     Set up the logger. Note that the setting will also impact the default logging logger, which means that simply
     using logging.info() will output the logs to both stdout and the filename_log.
     :param path_log: the filename of the log
+    :param force_add_handler: if True, will clear logging.root.handlers
     :type path_log: str
     """
     ret_logger = logging.getLogger()
     ret_logger.setLevel(logging.DEBUG)
     formatter = logging.Formatter('%(asctime)s-%(name)s-%(levelname)s: %(message)s', datefmt='%Y-%m-%d-%H:%M:%S')
+
+    if force_add_handler:
+        ret_logger.handlers = []
 
     if not ret_logger.handlers:
         path_log = "%s.log" % path_log if not path_log.endswith(".log") else path_log
