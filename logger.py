1494178fea011632e7dce55efbd8dc1817841149
ghzhang <zhangguanhua1996@gmail.com>
2023-04-22 20:31:06 +0800

    update logger

--- logger.py
+++ logger.py
@@ -4,7 +4,7 @@ import sys
 from zarth_utils.general_utils import get_random_time_stamp
 
 
-def get_logger(path_log="%s.log" % get_random_time_stamp(), force_add_handler=False):
+def get_logger(path_log="%s.log" % get_random_time_stamp(), force_add_handler=True):
     """
     Set up the logger. Note that the setting will also impact the default logging logger, which means that simply
     using logging.info() will output the logs to both stdout and the filename_log.
@@ -20,24 +20,23 @@ def get_logger(path_log="%s.log" % get_random_time_stamp(), force_add_handler=Fa
         ret_logger.handlers = []
 
     if not ret_logger.handlers:
-        path_log = "%s.log" % path_log if not path_log.endswith(".log") else path_log
-        fh = logging.FileHandler(path_log)
-        fh.setLevel(logging.DEBUG)
-        fh.setFormatter(formatter)
+        if path_log is not None:
+            path_log = "%s.log" % path_log if not path_log.endswith(".log") else path_log
+            fh = logging.FileHandler(path_log)
+            fh.setLevel(logging.DEBUG)
+            fh.setFormatter(formatter)
+            ret_logger.addHandler(fh)
 
         ch = logging.StreamHandler(sys.stdout)
         ch.setLevel(logging.DEBUG)
         ch.setFormatter(formatter)
-
         ret_logger.addHandler(ch)
-        ret_logger.addHandler(fh)
 
     return ret_logger
 
 
 def logging_info(*args):
     if logging.root.level > logging.getLevelName("INFO"):
-        logging.warning("Logging level higher than INFO!")
-        print(*args)
+        get_logger(path_log=None)
     else:
         logging.info(*args)
